0a7bccbf7f76e3d64c948326a15f84176ac0e0b9
Akshay Kumar <akgunner0910@gmail.com>
2026-01-19 00:44:45 +0530

    Initial commit

--- playwright/main.js
+++ playwright/main.js
@@ -133,3 +133,48 @@ processBatches([
   "34060",
   "55705",
 ]);
+
+function getElementsBetween(temp1, temp2) {
+  function splits(url) {
+    const segments = url.split(`/`);
+    for (let i = 0; i < segments.length; i++) {
+      let segment = segments[i];
+      if (!isNaN(segment) && segment) {
+        return segment;
+      }
+    }
+    return "";
+  }
+  // Ensure temp1 is before temp2 in the DOM
+  if (temp1.compareDocumentPosition(temp2) & Node.DOCUMENT_POSITION_PRECEDING) {
+    [temp1, temp2] = [temp2, temp1];
+  }
+
+  const elementsInBetween1 = [],
+    elementsInBetween2 = [];
+  let currentNode = temp1.nextElementSibling;
+
+  while (currentNode && currentNode !== temp2) {
+    if (currentNode.querySelectorAll(`a img`) != null) {
+      for (let imgElement of currentNode.querySelectorAll(`a img`)) {
+        const teamId = splits(imgElement.getAttribute(`src`));
+        elementsInBetween1.push(teamId);
+      }
+    }
+    currentNode = currentNode.nextElementSibling;
+  }
+
+  currentNode = temp1.nextElementSibling;
+
+  while (currentNode && currentNode !== temp2) {
+    if (currentNode.querySelectorAll(`a img`) != null) {
+      for (let imgElement of currentNode.querySelectorAll(`a img`)) {
+        const teamId = splits(imgElement.getAttribute(`src`));
+        elementsInBetween2.push(teamId);
+      }
+    }
+    currentNode = currentNode.nextElementSibling;
+  }
+
+  return Array.from(new Set([...elementsInBetween1, ...elementsInBetween2]));
+}
